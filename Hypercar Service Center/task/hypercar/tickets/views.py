12f9113bfb0925e51a8bf43508ffa3054d25c32e
VitaliiBandyl <vitaliibandyl@gmail.com>
2020-06-09 21:25:19 +0300

    Implemented Customer menu

--- Hypercar Service Center/task/hypercar/tickets/views.py
+++ Hypercar Service Center/task/hypercar/tickets/views.py
@@ -1,7 +1,22 @@
 from django.views import View
 from django.http.response import HttpResponse
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+services = {
+    'change_oil': 'Change oil',
+    'inflate_tires': 'Inflate tires',
+    'diagnostic': 'Get diagnostic test'
+}
 
 
 class WelcomeView(View):
+
     def get(self, request, *args, **kwargs):
         return HttpResponse('<h2>Welcome to the Hypercar Service!</h2>')
+
+
+class MenuView(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'tickets/menu.html', context={'services': services})
